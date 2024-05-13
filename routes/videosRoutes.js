0a7089349f4256bc7653b4e727038db7fd308159
Benjamin Ayesu-Attah <ben.ayesu.attah@gmail.com>
2024-05-13 14:37:41 -0700

    add post method and add video to videos.json

---
+++ routes/videosRoutes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+router.get("/", (req, res) => {
+  res.send("Hello Video Benjamin");
+});
+
+// Gets all videos
+router.get("/videos", (req, res) => {
+  const videos = JSON.parse(fs.readFileSync("./data/videos.json"));
+  res.send(videos);
+});
+
+// Gets a videos
+router.get("/videos/:id", (req, res) => {
+  const videos = JSON.parse(fs.readFileSync("./data/videos.json"));
+  const findVideo = videos.find((video) => video.id === req.params.id);
+  res.send(findVideo);
+});
+
+// Adds video to videos.json
+router.post("/videos", (req, res) => {
+  const newVideo = {
+    id: uuidv4(),
+    image: "default.jpeg",
+    reviews: [],
+  };
+  console.log(req.body);
+  res.send("This has been posted");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors = require("cars");
+const cors = require("cors");
 const videoRoutes = require("./routes/videosRoutes");
-const { PORT, BACKEND_URL } = process.env;
+const { PORT } = process.env;
 
 app.use(express.json());
 app.use(express.static("public"));
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to brainflix");
-});
+app.use(videoRoutes);
 
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
