caf083da9aef01f755a77f306b7737e099e435dc
Benjamin Ayesu-Attah <ben.ayesu.attah@gmail.com>
2024-05-14 13:39:47 -0700

    fixed route for post method

--- routes/videosRoutes.js
+++ routes/videosRoutes.js
@@ -23,13 +23,30 @@ router.get("/videos/:id", (req, res) => {
 
 // Adds video to videos.json
 router.post("/videos", (req, res) => {
+  const { title, description, image } = req.body;
   const newVideo = {
     id: uuidv4(),
-    image: "default.jpeg",
-    reviews: [],
+    title: title,
+    description: description,
+    image: image || "../images/image01",
+    views: "0",
+    likes: "0",
+    duration: "0:00",
+    video: "https://example.com/video.mp4",
+    timeStamp: Date.now(),
+    comments: [],
   };
-  console.log(req.body);
-  res.send("This has been posted");
+
+  try {
+    const data = fs.readFileSync("../data/videos.json");
+    const videos = JSON.parse(data);
+    videos.push(newVideo);
+    fs.writeFileSync("../data/videos.json", JSON.stringify(videos));
+    res.status(201).json(newVideo);
+  } catch (error) {
+    console.log("error posting new videos", error);
+    res.send("This has been posted");
+  }
 });
 
 module.exports = router;
