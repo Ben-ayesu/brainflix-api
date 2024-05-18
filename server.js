de66d225967d8dc0b3689070e204da1820f2833f
Benjamin Ayesu-Attah <ben.ayesu.attah@gmail.com>
2024-05-18 11:50:45 -0700

    final commit before submission

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const videoRoutes = require("./routes/videosRoutes");
 const { PORT } = process.env;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const videoRoutes = require("./routes/videosRoutes");
 
 app.use(express.json());
 app.use(express.static("public"));
-
 app.use(cors());
 
 app.use(videoRoutes);
